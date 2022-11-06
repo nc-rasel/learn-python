84a4fa4c22db66a2bbcf01d103f5615a4270a553
S.M. Rasel Mahamud <raselux@gmail.com>
2022-11-06 23:56:05 +0600

    first commit

---
+++ condition-logic.py
@@ -0,0 +1,16 @@
+# পাইথন প্রোগ্রামিং - লেকচার ৫ - কন্ডিশনাল লজিক ১
+
+nearly_pi = 3.141592653589793238462643383279502884197169399375105820974944
+print(nearly_pi)
+print(type(nearly_pi))
+
+almost_pi = 22/7
+print(almost_pi)
+print(type(almost_pi))
+
+marks = 35
+
+if marks >= 40:
+    print("pass")
+else:
+    print("fail")
