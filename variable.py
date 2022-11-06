0056344c215aa5e894fe2af64e88177a02e24ed6
S.M. Rasel Mahamud <raselux@gmail.com>
2022-11-06 15:01:22 +0600

    complete variable and data type python

---
+++ variable.py
@@ -0,0 +1,34 @@
+# পাইথন প্রোগ্রামিং - লেকচার ৪ - ভ্যারিয়েবল ও ডেটা টাইপ
+
+
+a = 5
+print (type(a))
+my_var = "rasel"
+print(type(my_var))
+
+b = 4
+print (a, b, my_var, "Hello Bangladesh")
+
+a0 = 5
+a_b = 6
+
+print (a0 + a_b)
+
+n1 = 6
+n2 = 2
+n3 = n1 ** n2
+print(n3)
+n1 = 4
+print(n3)
+
+n3 = n1 ** n2
+print(n3)
+
+# n = input("Enter number: ")
+# n = int(n)
+
+# if (n == 0 or n > 0):
+#     print(n, "is positive")
+# else:
+#     print(n, "is negetive number")
+
