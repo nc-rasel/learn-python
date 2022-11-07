6c5a5f35d34b128aff131eb9336cb50982d83415
S.M. Rasel Mahamud <raselux@gmail.com>
2022-11-07 14:34:29 +0600

    completed practise for conditional logic 3 lec 7

---
+++ conditional-logic-3.py
@@ -0,0 +1,26 @@
+# পাইথন প্রোগ্রামিং - লেকচার 7 - কন্ডিশনাল লজিক 3
+# Basic True False logic practise boolean data type
+#
+
+a = 3
+# print(type(a))
+if a == a:
+    print("True")
+else:
+    print("False")
+
+if a == 3:
+    print("True")
+else:
+    print("False")
+
+s1 = "Bangladesh"
+s2 = "bnagladesh"
+
+if s1 == s2:
+    print(True)
+else:
+    print("False")
+
+
+
