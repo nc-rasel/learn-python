1cdd2da1f4775bc6d1a29e60c0fd46402bb14102
Rasel Mahamud <raselux@gmail.com>
2022-11-08 12:53:45 +0600

    completed practise for library function

---
+++ function-2.py
@@ -0,0 +1,22 @@
+# পাইথন প্রোগ্রামিং - লেকচার ১২ - লাইব্রেরি ফাংশন
+# print random number range 1 to 100 using random library
+
+import random
+import datetime
+import math
+
+
+random_number = random.randint(1, 100)
+print(random_number)
+
+
+today = datetime.datetime.today()
+
+print(today)
+
+x = math.sqrt(100)
+print(x)
+
+pi = math.pi
+print(pi)
+
