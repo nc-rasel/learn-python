d67e84d53e8470d67328a07734683ab9ef1a654c
Rasel Mahamud <raselux@gmail.com>
2022-11-08 12:10:51 +0600

    completed practise for multipication table using function

---
+++ function-1.py
@@ -0,0 +1,26 @@
+# পাইথন প্রোগ্রামিং - লেকচার ১১ - ফাংশন - ১
+# Genrate a multipication table using function
+
+
+def generate_multipication_table(n):
+    print(n, "x 1 =", n * 1)
+    print(n, "x 2 =", n * 2)
+    print(n, "x 3 =", n * 3)
+    print(n, "x 4 =", n * 4)
+    print(n, "x 5 =", n * 5)
+    print(n, "x 6 =", n * 6)
+    print(n, "x 7 =", n * 7)
+    print(n, "x 8 =", n * 8)
+    print(n, "x 9 =", n * 9)
+    print(n, "x 10 =", n * 10)
+    # return None
+
+n = input("Enter numer: ")
+n = int(n)
+value = generate_multipication_table(n)
+print(value)
+
+
+
+
+
