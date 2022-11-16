c5285539a366430a1ddb859439522ab98c553219
Rasel Mahamud <raselux@gmail.com>
2022-11-16 22:18:02 +0600

    update string class

--- string.py
+++ string.py
@@ -1,3 +1,4 @@
+# https://www.youtube.com/watch?v=B6oKPK2SNxk
 # s = "hello"
 # len(s)
 
@@ -13,4 +14,25 @@
 # len(s)
 
 country = "Bangladesh"
+
 print(country[4])
+
+print(len(country))
+
+print(country.upper())
+
+print(country.lower())
+
+print(country.capitalize())
+
+country = "Bangladesh"
+print(country.find('desh'))
+print(country.startswith('desh'))
+print(country.endswith('desh'))
+
+# string is immutable 
+
+s = "A B C D E"
+s.split(",")
+print(s)
+print("+".join(s))
