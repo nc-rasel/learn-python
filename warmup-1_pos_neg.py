12da757d52159f5850b8966c812fc9a6ef592ecd
Rasel Mahamud <raselux@gmail.com>
2022-11-17 22:51:12 +0600

    Warmup-1 pos_neg

---
+++ warmup-1_pos_neg.py
@@ -0,0 +1,15 @@
+# Given 2 int values, return True if one is negative and one is positive. Except if the parameter "negative" is True, then return True only if both are negative.
+
+# pos_neg(1, -1, False) → True
+# pos_neg(-1, 1, False) → True
+# pos_neg(-4, -5, True) → True
+
+def pos_neg(a, b, negative):
+  if (a >= 0 and b < 0) and (negative == False):
+    return True
+  elif (a < 0 and b >= 0) and (negative == False):
+    return True
+  elif (a < 0 and b < 0) and (negative is True):
+    return True
+  else:
+    return False
