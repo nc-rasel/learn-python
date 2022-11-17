762545856e98c9678aa51cae63a26bc8b43d793a
Rasel Mahamud <raselux@gmail.com>
2022-11-17 22:47:57 +0600

    practise not string style 2

--- warmup-1_not_string.py
+++ warmup-1_not_string.py
@@ -10,3 +10,10 @@ def not_string(str):
         return "not " + x
     else:
         return x
+
+# def not_string(str):
+#   if len(str) >= 3 and str[:3] == "not":
+#     return str
+#   return "not " + str
+  # str[:3] goes from the start of the string up to but not
+  # including index 3
